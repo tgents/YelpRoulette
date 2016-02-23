17e50d882df5a1052250b49d27e7bf9a29ee0829
Jeff Zhong <jz.liberation@gmail.com>
2016-02-23 14:43:32 -0800

    Very basic start to home page. Will find some cool libraries to make this better

--- finalproject/app/src/main/java/stooges/three/finalproject/MainActivity.java
+++ finalproject/app/src/main/java/stooges/three/finalproject/MainActivity.java
@@ -2,6 +2,8 @@ package stooges.three.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner) findViewById(R.id.filter_distance);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.distance, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
