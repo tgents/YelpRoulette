ae6782774ae65945497b13d024a1d54ced8d6b9f
Jeff Zhong <jz.liberation@gmail.com>
2016-02-27 18:18:47 -0800

    Added a circular button lib, and added listeners for the button and spinner. Gonna need more listeners for each spinner.

--- finalproject/app/src/main/java/stooges/three/finalproject/MainActivity.java
+++ finalproject/app/src/main/java/stooges/three/finalproject/MainActivity.java
@@ -2,22 +2,56 @@ package stooges.three.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import com.dd.CircularProgressButton;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    CircularProgressButton circle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Spinner spinner = (Spinner) findViewById(R.id.filter_distance);
-// Create an ArrayAdapter using the string array and a default spinner layout
+        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.distance, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+        // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        circle = (CircularProgressButton) findViewById(R.id.search_button);
+        circle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Within this method, call the async task that connects to Yelp and pulls restaurant data
+                if(circle.isIndeterminateProgressMode()) {
+                    circle.setIndeterminateProgressMode(false);
+                } else {
+                    circle.setIndeterminateProgressMode(true);
+                }
+            }
+        });
     }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+
 }
+
+//Instructions for messing with the circular button: https://github.com/dmytrodanylyk/circular-progress-button/wiki/User-Guide
