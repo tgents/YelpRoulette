12c12ac58c0fb9ffa1c711681507f83fbb5603da
s3and0s <s3and0s@hotmail.com>
2016-03-01 16:43:18 -0800

    Sean

--- finalproject/app/src/main/java/stooges/three/finalproject/MainActivity.java
+++ finalproject/app/src/main/java/stooges/three/finalproject/MainActivity.java
@@ -2,56 +2,69 @@ package stooges.three.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.dd.CircularProgressButton;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
-    CircularProgressButton circle;
+    private static final String TAG = "MainActivity";
+    CircularProgressButton maincircle;
+    CircularProgressButton favcircle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner spinner = (Spinner) findViewById(R.id.filter_distance);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.distance, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
 
-        circle = (CircularProgressButton) findViewById(R.id.search_button);
-        circle.setOnClickListener(new View.OnClickListener() {
+
+        maincircle = (CircularProgressButton) findViewById(R.id.search_button);
+        maincircle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Within this method, call the async task that connects to Yelp and pulls restaurant data
-                if(circle.isIndeterminateProgressMode()) {
-                    circle.setIndeterminateProgressMode(false);
+                if (maincircle.isIndeterminateProgressMode()) {
+                    maincircle.setIndeterminateProgressMode(false);
+                    maincircle.setProgress(0);
                 } else {
-                    circle.setIndeterminateProgressMode(true);
+                    maincircle.setIndeterminateProgressMode(true);
+                    maincircle.setProgress(50); // set progress > 0 & < 100 to display indeterminate progress
                 }
             }
         });
-    }
 
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
-        // An item was selected. You can retrieve the selected item using
-        // parent.getItemAtPosition(pos)
-    }
-
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
+        favcircle = (CircularProgressButton) findViewById(R.id.favorites_search);
+        favcircle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Within this method, call the async task that will pull restaurant from favorites list
+                if(favcircle.isIndeterminateProgressMode()) {
+                    favcircle.setIndeterminateProgressMode(false);
+                    favcircle.setProgress(0);
+                } else {
+                    favcircle.setIndeterminateProgressMode(true);
+                    favcircle.setProgress(50); // set progress > 0 & < 100 to display indeterminate progress
+                }
+            }
+        });
     }
 
 
 }
 
 //Instructions for messing with the circular button: https://github.com/dmytrodanylyk/circular-progress-button/wiki/User-Guide
+
+//Spinner spinner = (Spinner) findViewById(R.id.filter_distance);
+//// Create an ArrayAdapter using the string array and a default spinner layout
+//ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//        R.array.distance, android.R.layout.simple_spinner_item);
+//// Specify the layout to use when the list of choices appears
+//adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        // Apply the adapter to the spinner
+//        spinner.setAdapter(adapter);
+//        spinner.setOnItemSelectedListener(this);
