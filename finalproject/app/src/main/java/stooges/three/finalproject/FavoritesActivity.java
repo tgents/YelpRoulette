d5d192c60990b4a036cb5102800bde5dc8486e75
tytseng <thomasytseng@gmail.com>
2016-03-01 16:04:05 -0800

    yelp url

--- finalproject/app/src/main/java/stooges/three/finalproject/FavoritesActivity.java
+++ finalproject/app/src/main/java/stooges/three/finalproject/FavoritesActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class FavoritesActivity extends AppCompatActivity {
 
+    private static final String API_HOST = "api.yelp.com";
+    private static final String SEARCH_PATH = "/v2/search";
+    
     public static final String consumer = "7EPO_0MB-5XsfzeE5PpqRw";
     public static final String consumer_secret = "5BYGwh_WC9AdJL6_dKFYr1JKCVc";
     public static final String token = "pXGPbhiaGJ7YiXADvT5uxSOSAFqGzKVy";
