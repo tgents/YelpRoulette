9fbc7aab4f2b879179f37d65e5120a3a9cabfd76
s3and0s <s3and0s@hotmail.com>
2016-03-04 15:04:03 -0800

    Sean

--- finalproject/app/src/main/java/stooges/three/finalproject/RestaurantDetailActivity.java
+++ finalproject/app/src/main/java/stooges/three/finalproject/RestaurantDetailActivity.java
@@ -3,11 +3,19 @@ package stooges.three.finalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Created by Sean Ker on 3/1/2016
+ */
+
 public class RestaurantDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+
+
     }
+
+
 }
