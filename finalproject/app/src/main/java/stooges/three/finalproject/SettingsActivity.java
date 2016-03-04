a41433c0d2dcdbb63feb224b068a05328d9abe52
FXStrato <jz.liberation@gmail.com>
2016-03-04 15:38:17 -0800

    Added working settings actvity

---
+++ finalproject/app/src/main/java/stooges/three/finalproject/SettingsActivity.java
@@ -0,0 +1,14 @@
+package stooges.three.finalproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+}
