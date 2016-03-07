248e8aa73a650bd1a562a4df372591ca7183b962
FXStrato <jz.liberation@gmail.com>
2016-03-06 19:44:21 -0800

    Moved yelp api call to main screen, and now hit me button starts restaurant details activity with an intent

--- finalproject/app/src/main/java/stooges/three/finalproject/RestaurantDetailActivity.java
+++ finalproject/app/src/main/java/stooges/three/finalproject/RestaurantDetailActivity.java
@@ -1,7 +1,9 @@
 package stooges.three.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 /**
@@ -10,12 +12,18 @@ import android.widget.Button;
 
 public class RestaurantDetailActivity extends AppCompatActivity {
 
+    private static final String TAG = "RestaurantActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
 
         Button searchButton = (Button) findViewById(R.id.search_button);
+
+        //Information for restaurants are in this. They are "name", "address", "image", "url", and "rating"
+        Intent intent = getIntent();
+        Log.v(TAG, "Intent was received. Can begin inserting information onto screen");
 
 
 
