546a2bc408f1d9efa37ce0161df413ee2d99ebdb
Jeff Zhong <jz.liberation@gmail.com>
2016-03-09 23:06:31 -0800

    My changes

--- finalproject/app/src/main/java/stooges/three/finalproject/MainActivity.java
+++ finalproject/app/src/main/java/stooges/three/finalproject/MainActivity.java
@@ -122,7 +122,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                         String distance = sharedPref.getString("pref_distance", "");
 //                        Log.v(TAG, distance);
 //                        Log.v(TAG, "latlon:" + lat + "," +lon);
-                        new YelpApi().execute("restaurant", lat + "", lon + "", dist + "");
+                        new YelpApi().execute("restaurant", lat + "", lon + "", distance + "");
 //                        if (distance != "")
 //                            new YelpApi().execute("restaurant", lat + "", lon + "", distance + "");
 //                        else new YelpApi().execute("restaurant", lat + "", lon + "", dist + "");
